1892f9d11e0d3f3bf51302578606f174e55031b9
Fernanda Hallmann <111521249+FernandaHallmann@users.noreply.github.com>
2023-05-10 19:25:43 -0300

    Update Gato.java

--- ExercicioRedefinicao/src/main/java/com/mycompany/exercicioredefinicao/Gato.java
+++ ExercicioRedefinicao/src/main/java/com/mycompany/exercicioredefinicao/Gato.java
@@ -5,7 +5,7 @@ package com.mycompany.exercicioredefinicao;
  * @author Ferna
  */
 public class Gato extends Animal {
-    @Override
+
     public void fazerBarulho() {
         System.out.println("O gato está miando");
     }
