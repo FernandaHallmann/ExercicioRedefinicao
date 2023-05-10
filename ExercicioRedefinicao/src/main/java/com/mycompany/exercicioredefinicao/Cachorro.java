c4465bb23f44473504dff2a9387c97af17f1c004
Fernanda Hallmann <111521249+FernandaHallmann@users.noreply.github.com>
2023-05-10 19:25:26 -0300

    Update Cachorro.java

--- ExercicioRedefinicao/src/main/java/com/mycompany/exercicioredefinicao/Cachorro.java
+++ ExercicioRedefinicao/src/main/java/com/mycompany/exercicioredefinicao/Cachorro.java
@@ -5,7 +5,7 @@ package com.mycompany.exercicioredefinicao;
  * @author Ferna
  */
 public class Cachorro extends Animal {
-    @Override
+
     public void fazerBarulho() {
         System.out.println("O cachorro está latindo");
     }
